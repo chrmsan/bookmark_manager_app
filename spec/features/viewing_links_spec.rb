2b48442ca92ec1301ddaed880a2405c0343fcd1a
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-17 15:16:24 +0100

    Correct DataMapper command to Link model from new to create in spec

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 feature 'Viewing links' do
 
 	scenario 'links are visible on the /links endpoint' do
-		Link.new(url: 'http://www.google.com', title: 'Google Homepage')
+		Link.create(url: 'http://www.google.com', title: 'Google Homepage')
+		
 
 		visit '/links'
 		expect(page.status_code).to eq(200)
