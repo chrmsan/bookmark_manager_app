7f3f77a602555dafc7fcdc6fbd201af381c7b3fa
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-21 17:14:34 +0100

    Add Database_cleaner configuration in spec_helper in order to clear the database after each test run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,10 +8,26 @@ require File.join(File.dirname(__FILE__), '..', 'lib/my_app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'database_cleaner'
 
 Capybara.app = MyApp
 
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+
   config.include Capybara::DSL
 
   config.expect_with :rspec do |expectations|
