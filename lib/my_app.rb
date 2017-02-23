25a7542d3861d6da3eaeb82dcd866678fe96e001
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-23 17:35:19 +0100

    Add get tags/:name parameter endpoint that sorts tags by name

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -21,5 +21,11 @@ class MyApp < Sinatra::Base
     redirect '/links'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
   run! if app_file == $0
 end
