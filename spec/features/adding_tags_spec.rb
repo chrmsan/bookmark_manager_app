28dab7d84d3e9790604e234f3ece81011f6e94e1
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-02-25 13:52:08 +0100

    Add test where you can add several tags to a link

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -11,7 +11,17 @@ feature 'Adding tags' do
 		click_button 'Add Link'
 		link = Link.first #it aquires the the first instance in the Link database
 		expect(link.tags.map(&:name)).to include('pikachu')
+	end
 
+	scenario 'Adding multiple tags to a new link' do
+		visit '/links/new'
+		fill_in 'title', with: 'A Kitchn Site'
+		fill_in 'url', with: 'http://www.thekitchn.com/'
+		fill_in 'tags', with: 'Food Hipster'
+
+		click_button 'Add Link'
+		link = Link.first
+		expect(link.tags.map(&:name)).to include('Food', 'Hipster')
 	end
 
 
