43858d79ecb81c82832139d24bca8fe64e80a518
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 15:50:52 +0100

    Refactor sign_up method by adding user info as method parameter

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,8 @@
-def sign_up
+def sign_up(email: "john@doe.com", password: "apples", password_confirmation: "apples")
 	visit '/users/new'
 	expect(page.status_code).to eq(200)
-	fill_in :email,		with: "john@doe.com"
-	fill_in :password, with: "apples"
+	fill_in :email,		with: email
+	fill_in :password, with: password
+	fill_in :password_confirmation, with: password_confirmation
 	click_button "Sign up"
 end
