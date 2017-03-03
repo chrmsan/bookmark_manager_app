74af39145a9139363e08058e2912b6f8525c730b
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 14:57:11 +0100

    Enable sessions and session_secret

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -4,6 +4,9 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class MyApp < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super_secret'
+  
   get '/links' do
     @links = Link.all
     erb :'links/index'
