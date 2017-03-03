bfe9959e7c091123bce137a240d6c123b142c239
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 14:19:50 +0100

    Fixed typo in rspec method

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,6 +1,6 @@
 def sign_up
 	visit '/users/new'
-	expect(page.status_code).to ew(200)
+	expect(page.status_code).to eq(200)
 	fill_in :email,		with: "john@doe.com"
 	fill_in :password, with: "apples"
 	click_button "Sign up"
