3a7939aea82713be906cd346b9ca2f88a2a8cb82
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 17:27:17 +0100

    Add test for wrong password confirmation at sign up

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,9 +1,15 @@
 require "spec_helper"
 
 feature "User sign up" do
+
 	scenario "I can sign up as a new user" do
 		expect { sign_up }.to change(User, :count).by(1)
 		expect(page).to have_content("Welcome, john@doe.com")
 		expect(User.first.email).to eq("john@doe.com")
 	end
+
+	scenario "requires a matchong confirmation password" do
+		expect { sign_up(password_confirmation: "wrong") }.not_to change(User, :count)
+	end
+
 end
