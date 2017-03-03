a753bee4bbdb6e64555acb181bb768b994893596
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 19:10:20 +0100

    Add sinatra flash and use @user instance in post /user endpoint

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -1,11 +1,15 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class MyApp < Sinatra::Base
+
   enable :sessions
   set :session_secret, 'super_secret'
+  
+  register Sinatra::Flash
   
   get '/links' do
     @links = Link.all
@@ -32,13 +36,19 @@ class MyApp < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/links'
+    @user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/links'
+    else
+      flash.now[:notice] = "Password and confirmation does not match"
+      erb:'users/new'
+    end
   end
 
   helpers do
