1eaa9f73853eb124c06e1f9e556eed04ec06110d
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 18:25:32 +0100

    Add test showing messages at wrong password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,8 +8,10 @@ feature "User sign up" do
 		expect(User.first.email).to eq("john@doe.com")
 	end
 
-	scenario "requires a matchong confirmation password" do
+	scenario "requires a matching confirmation password" do
 		expect { sign_up(password_confirmation: "wrong") }.not_to change(User, :count)
+		expect(current_path).to eq("/users")
+		expect(page).to have_content("Password and confirmation does not match")
 	end
 
 end
