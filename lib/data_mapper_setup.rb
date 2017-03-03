0341d51cb6f1318f9ea3e4cb7639302684a95d1c
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 17:40:31 +0100

    Rename filename from Models to Users

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -3,7 +3,7 @@ require 'dm-postgres-adapter'
 
 require_relative 'models/links'
 require_relative 'models/tags'
-require_relative 'models/models'
+require_relative 'models/users'
 
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
