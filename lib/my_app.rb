749e726e770a3b3a26693c28711d65a112f2b7c4
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 14:32:10 +0100

    Add get/new and post  methods for users

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -28,5 +28,14 @@ class MyApp < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    User.create(email: params[:email], password: params[:password])
+    redirect '/links'
+  end
+
   run! if app_file == $0
 end
