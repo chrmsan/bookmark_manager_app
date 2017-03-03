0ea136cd1bb5267315aa79d82a59feccad0eb4ea
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-03 21:10:40 +0100

    Correct expected error messages in tests due to flash[:errors]

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -11,15 +11,19 @@ feature "User sign up" do
 	scenario "requires a matching confirmation password" do
 		expect { sign_up(password_confirmation: "wrong") }.not_to change(User, :count)
 		expect(current_path).to eq("/users")
-		expect(page).to have_content("Password and confirmation does not match")
+		expect(page).to have_content("Password does not match the confirmation")
 	end
 
 	scenario "I can't sign up without an email address" do
 		expect { sign_up(email: nil) }.not_to change(User, :count)
+		expect(current_path).to eq("/users")
+		expect(page).to have_content("Email must not be blank")
 	end
 
 	scenario "I can't sign up with wrong email address" do
 		expect { sign_up(email: "john@john") }.not_to change(User, :count)
+		expect(current_path).to eq("/users")
+		expect(page).to have_content("Email has an invalid format")
 	end
 
 	scenario "I can't sign up again with same email address" do
