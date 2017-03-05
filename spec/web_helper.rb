de9958cbc8fcbbf2b03a31be343fd80a5549d77d
Christopher Sanchez <sanchez.christopherm@gmail.com>
2017-03-05 15:49:38 +0100

    Add webhelper method for sign in

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,3 +6,10 @@ def sign_up(email: "john@doe.com", password: "apples", password_confirmation: "a
 	fill_in :password_confirmation, with: password_confirmation
 	click_button "Sign up"
 end
+
+def sign_in(email:, password:)
+	visit '/session/new'
+	fill_in :email, with: email
+	fill_in :password, with: password
+	click_button "Sign in"
+end
